977e9dc656d55dd5673bba634e4ac6c72ec4f2e0
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-08-17 11:27:18 +0700

    Add examples for traditional/standard ELM models

--- examples/exam_standard_elm_regression.py
+++ examples/exam_standard_elm_regression.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-# Created by "Thieu" at 22:28, 14/08/2023 ----------%
+# Created by "Thieu" at 10:12, 17/08/2023 ----------%                                                                               
 #       Email: nguyenthieu2102@gmail.com            %                                                    
 #       Github: https://github.com/thieu1995        %                         
 # --------------------------------------------------%
 
 import numpy as np
-from intelelm import get_dataset, MhaElmRegressor
+from intelelm import get_dataset, ElmRegressor
 
 
 data = get_dataset("diabetes")
@@ -18,8 +18,7 @@ data.X_test = scaler_X.transform(data.X_test)
 data.y_train, scaler_y = data.scale(data.y_train, method="MinMaxScaler", feature_range=(0, 1))
 data.y_test = scaler_y.transform(np.reshape(data.y_test, (-1, 1)))
 
-opt_paras = {"name": "GA", "epoch": 100, "pop_size": 30}
-model = MhaElmRegressor(hidden_size=10, act_name="elu", obj_name="NNSE", optimizer="BaseGA", optimizer_paras=opt_paras)
+model = ElmRegressor(hidden_size=10, act_name="elu")
 model.fit(data.X_train, data.y_train)
 
 pred = model.predict(data.X_test)
