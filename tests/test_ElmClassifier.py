750b4c2d4c2ca267cd977415a16dcd54bd339d36
Thieu Nguyen <nguyenthieu2102@gmail.com>
2023-08-17 11:29:50 +0700

    Add tests for standard ELM models

---
+++ tests/test_ElmClassifier.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 11:27, 17/08/2023 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+import numpy as np
+from intelelm import ElmClassifier
+
+np.random.seed(41)
+
+
+def test_ElmClassifier_class():
+    X = np.random.rand(100, 6)
+    y = np.random.randint(0, 2, size=100)
+
+    model = ElmClassifier(hidden_size=10, act_name="elu")
+    model.fit(X, y)
+    pred = model.predict(X)
+    assert ElmClassifier.SUPPORTED_CLS_METRICS == model.SUPPORTED_CLS_METRICS
+    assert pred[0] in (0, 1)
---
+++ tests/test_ElmRegressor.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# Created by "Thieu" at 11:25, 17/08/2023 ----------%                                                                               
+#       Email: nguyenthieu2102@gmail.com            %                                                    
+#       Github: https://github.com/thieu1995        %                         
+# --------------------------------------------------%
+
+import numpy as np
+from intelelm import ElmRegressor
+
+np.random.seed(42)
+
+
+def test_ElmRegressor_class():
+    X = np.random.uniform(low=0.0, high=1.0, size=(100, 5))
+    noise = np.random.normal(loc=0.0, scale=0.1, size=(100, 5))
+    y = 2 * X + 1 + noise
+
+    model = ElmRegressor(hidden_size=10, act_name="elu")
+    model.fit(X, y)
+
+    pred = model.predict(X)
+    assert ElmRegressor.SUPPORTED_REG_METRICS == model.SUPPORTED_REG_METRICS
+    assert len(pred) == X.shape[0]
